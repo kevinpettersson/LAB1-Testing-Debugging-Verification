f53f6fa255d34ca1468a4abe9d0569cad2fe2409
Kevin Pettersson <kevin.petterssons@hotmail.com>
2024-11-04 16:09:33 +0100

    fixa så det funka på intelliJ

---
+++ ex1/SetTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetTests {
+
+   @Test 
+   public void toArray() {
+       assertEquals(4,4);
+  }
+    
+}
